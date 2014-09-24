b233918e347c28e1a7f90f4dd53a2d3ddc44f4bf
Michael Noseworthy <mnoseworthy7@gmail.com>
2014-09-24 16:35:36 -0400

    Added tests for Card.hashCode().

--- test/ca/mcgill/cs/comp303/rummy/model/TestCard.java
+++ test/ca/mcgill/cs/comp303/rummy/model/TestCard.java
@@ -1,6 +1,40 @@
 package ca.mcgill.cs.comp303.rummy.model;
-
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
 
 public class TestCard
 {
+	
+	/**
+	 *  Tests Card.hashCode() to make sure cards only have the same hashcode if they have
+	 *  the same rank and suit.
+	 */
+	@Test 
+	public void testHashcode() 
+	{
+		// Make sure all cards have different hashcodes.
+		for (Card.Rank r : Card.Rank.values()) 
+		{
+			for (Card.Suit s : Card.Suit.values()) 
+			{
+				Card c = new Card(r, s);
+				for (Card.Rank r2 : Card.Rank.values()) 
+				{
+					for (Card.Suit s2 : Card.Suit.values()) 
+					{
+						Card c2 = new Card(r2, s2);
+						if (s2 == s && r2 == r)
+						{
+							assertEquals(c.hashCode(), c2.hashCode());
+						}
+						else 
+						{
+							assertFalse(c.hashCode() == c2.hashCode());
+						}
+					}
+				}
+			}
+		}
+	}
 }
