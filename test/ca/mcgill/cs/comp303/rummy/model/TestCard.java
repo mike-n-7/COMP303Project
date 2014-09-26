d1dd07abb4c96cff7d0adfa3e444fbd2e5bbb82b
Eric Quinn <unameriquinn@gmail.com>
2014-09-26 09:43:03 -0400

    Added testCard.java : testCompareTo()

--- test/ca/mcgill/cs/comp303/rummy/model/TestCard.java
+++ test/ca/mcgill/cs/comp303/rummy/model/TestCard.java
@@ -38,7 +38,7 @@ public class TestCard
 		}
 	}
 	/**
-	 *  Tests Card.equals() to make sure that only cards that have the same rank and suit
+	 *  Tests Card.compareTo() to make sure that only cards that have the same rank and suit
 	 *  are identified as equal.  
 	 */
 	@Test
@@ -62,6 +62,73 @@ public class TestCard
 						else 
 						{
 							assertFalse(c1.equals(c2));
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 *  Tests Card.equals() to make sure that cards with greater suits, or same suits and greater ranks are identified correctly.
+	 *  Additionally, the same cards are identified as equal. 
+	 */
+	@Test
+	public void testCompareTo()
+	{	
+		//basic tests
+		Card testC1 = new Card(Card.Rank.KING, Card.Suit.SPADES);
+		Card testC2 = new Card(Card.Rank.ACE, Card.Suit.SPADES);
+		Card testC3 = new Card(Card.Rank.KING, Card.Suit.CLUBS);
+		Card testC4 = new Card(Card.Rank.ACE, Card.Suit.CLUBS);
+		Card testC5 = new Card(Card.Rank.ACE, Card.Suit.CLUBS);
+		assertTrue(testC1.compareTo(testC2) > 0);
+		assertTrue(testC1.compareTo(testC3) > 0);
+		assertTrue(testC1.compareTo(testC4) > 0);
+		assertTrue(testC2.compareTo(testC3) > 0);
+		assertTrue(testC2.compareTo(testC4) > 0);
+		assertTrue(testC5.compareTo(testC5) == 0);
+		assertTrue(testC4.compareTo(testC3) < 0);
+		assertTrue(testC4.compareTo(testC2) < 0);
+		assertTrue(testC4.compareTo(testC1) < 0);
+		assertTrue(testC3.compareTo(testC2) < 0);
+		assertTrue(testC3.compareTo(testC1) < 0);
+		
+		for (Card.Rank r1 : Card.Rank.values()) 
+		{
+			for (Card.Suit s1 : Card.Suit.values()) 
+			{
+				Card c1 = new Card(r1, s1);
+				for (Card.Rank r2 : Card.Rank.values()) 
+				{
+					for (Card.Suit s2 : Card.Suit.values()) 
+					{
+						Card c2 = new Card(r2, s2);
+						if ((s1.ordinal() == s2.ordinal() && r1.ordinal() > r2.ordinal()) || s1.ordinal() > s2.ordinal())
+						{
+							assertTrue(c1.compareTo(c2) > 0);
+							assertFalse(c1.compareTo(c2) < 0);
+							assertFalse(c1.compareTo(c2) == 0);
+						}
+						else {
+							assertFalse(c1.compareTo(c2) > 0);
+						}
+						if((s1.ordinal() == s2.ordinal() && r1.ordinal() < r2.ordinal()) || s1.ordinal() < s2.ordinal()) 
+						{
+							assertTrue(c1.compareTo(c2) < 0);
+							assertFalse(c1.compareTo(c2) > 0);
+							assertFalse(c1.compareTo(c2) == 0);
+						}
+						else {
+							assertFalse(c1.compareTo(c2) < 0);
+						}
+						if (s1 == s2 && r1 == r2) {
+							assertTrue(c1.compareTo(c2) == 0);
+							assertFalse(c1.compareTo(c2) > 0);
+							assertFalse(c1.compareTo(c2) < 0);
+						}
+						else {
+							assertFalse(c1.compareTo(c2) == 0);
 						}
 					}
 				}
